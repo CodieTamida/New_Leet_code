c92442868e3ae4c6c2a28375271579eeca29be29
Codie Tamida <codietamida@gmail.com>
2023-11-12 21:09:22 -0800

    Added Screen shots from previous repo and Porject Proposal link in About,js

--- screens/About.js
+++ screens/About.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import {Context} from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, SafeAreaView} from 'react-native';
 import AppContext from './AppContext';
 import App from '../App';
+import ButtonLink from '../components/ButtonLink';
 //import Shake from './components/Shake';
 
 const About = ({navigation}) => {
   const context = React.useContext(AppContext);
 
   return (
-    <View>
+    <SafeAreaView>
       <Text>Codie Tamida Leetcode Final Project</Text>
-    </View>
+      <ButtonLink
+        url={`https://docs.google.com/spreadsheets/d/1QYkedl-cMrZvkkWW4SI0Lj2q0arkyPujfpoCpo9NX_c/edit?usp=sharing`}
+        title="Click Here for Project Proposal"
+      />
+      <Text>
+        Project Proposal:
+        https://docs.google.com/spreadsheets/d/1QYkedl-cMrZvkkWW4SI0Lj2q0arkyPujfpoCpo9NX_c/edit?usp=sharing
+      </Text>
+    </SafeAreaView>
   );
 };
 
